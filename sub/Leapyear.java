84704758bbd379d0115d604893bb4d9500ea7eec
Harithambika T H <harithambikat@gmail.com>
2022-07-26 12:15:08 +0530

    This Shows Leapyear or not - Leapyear.java

---
+++ sub/Leapyear.java
@@ -0,0 +1,15 @@
+class Leapyear
+{
+    public static void main(String are[]) 
+    {
+        int num=2016;
+       if(num%4==0)
+       {
+           System.out.println(num +"  is a Leap Year");
+       }
+       else
+       {
+           System.out.println(num +"  is not a Leap Year.");
+       } 
+    }
+}
